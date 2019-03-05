7cb3784f287b15b4c16cdd227cdc6f9898f63b5d
Magdalena Karpacka <karpackamagdalena@gmail.com>
2019-03-05 18:38:00 +0100

    testy wykonują się poprawnie

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -2,11 +2,30 @@ package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        if(value == null){
+            return Collections.emptyList();
+        }
+        List<String> listOfReturnedStrings = new ArrayList<>();
+
+        for(int i=0; i<value.length(); i++){
+            String temp = value.substring(i, value.length());
+            listOfReturnedStrings.add(temp);
+        }
+
+        listOfReturnedStrings.add("");
+
+        return listOfReturnedStrings;
+
+
+
+
     }
+
 
 }
